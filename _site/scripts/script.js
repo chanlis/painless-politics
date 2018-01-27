f485d552eebc9df39bb2dc82973806116cfb8912
meganla <meganlarson415@gmail.com>
2018-01-27 16:39:03 -0500

    first attempt at incorporating indico

--- _site/scripts/script.js
+++ _site/scripts/script.js
@@ -4,6 +4,8 @@ document.addEventListener('DOMContentLoaded', function () {
     addRightArticles('donald-trump');
 });
 
+// url array to be stored as a variable
+var desArr;
 
 // Returns a Promise that in turn returns an array of articles about the given search query
 function getArticles(topic) {
@@ -25,6 +27,28 @@ function getArticles(topic) {
             return res.data.articles;
 
         }))
+}
+
+// adds first 6 urls to an array of urls
+function buildDescriptionArray(topic) {
+    getArticles(topic).then(data) => {
+        for(var i = 0; i < 6; i++) {
+            desArr.push(data[i].description);
+        }
+    }
+}
+
+function rateDescriptions {
+    for (var i = 0; i < desArr.length; i++) {
+        $.post(
+        'https://apiv2.indico.io/political',
+         JSON.stringify({
+            'api_key': "5ca06cad390d60fb18f065997d116866",
+            'data': desArr[i],
+            'threshold': 0.25
+        })
+        ).then(function(res) { console.log(res) });
+}
 }
 
 // Adds articles to the left column related to the given search query
