29f611669eff23f804676a4cf6dac34b14cd654c
Caitlin Wang <caitlinwang97@gmail.com>
2018-01-27 12:40:36 -0500

    updated script to display two columns of articles

--- scripts/script.js
+++ scripts/script.js
@@ -1,9 +1,12 @@
+// Loads HTML DOM
+document.addEventListener('DOMContentLoaded', function () {
+    addLeftArticles('planned-parenthood');
+    addRightArticles('donald-trump');
+});
 
-// Example: planned parenthood
-getArticle('planned-parenthood')
 
-// A function I wrote to display an array of articles with the given topic/keywords
-function getArticle(topic) {
+// Returns a Promise that in turn returns an array of articles about the given search query
+function getArticles(topic) {
     var url = 'https://newsapi.org/v2/everything?' +
         'q=' + topic + '&' +
         'from=2018-01-01&' +
@@ -12,16 +15,53 @@ function getArticle(topic) {
 
     var req = new Request(url);
 
-    fetch(url).then(response =>
+    return fetch(url).then(response =>
         response.json().then(data => ({
             data: data,
             status: response.status
         })
         ).then(res => {
             // print array of articles to console
-            console.log(res.status, res.data.articles)
+            return res.data.articles;
+
         }))
 }
+
+// Adds articles to the left column related to the given search query
+function addLeftArticles(topic) {
+
+    // If you want to do something with the array of articles, use the following call
+    getArticles(topic).then((data) => {
+
+        // The parameter 'data' is the array of articles
+
+        var leftArticleList = "";
+
+        // Build HTML to list 3 most recent articles
+        for (var i = 0; i < 3; i++) {
+            leftArticleList = leftArticleList + "<a href=" + data[i].url + ", class=\'article\'><p>" + data[i].title + "</p></a>";
+        }
+
+        // Insert HTML in left div
+        document.getElementById("left").innerHTML = leftArticleList;
+    })
+}
+
+// Adds articles to the right column related to the given search query
+function addRightArticles(topic) {
+
+    getArticles(topic).then((data) => {
+        var rightArticleList = "";
+        for (var i = 0; i < 3; i++) {
+            rightArticleList = rightArticleList + "<a href=" + data[i].url + ", class=\'article\'><p>" + data[i].title + "</p></a>";
+        }
+
+        // Insert HTML in right div
+        document.getElementById("right").innerHTML = rightArticleList;
+    })
+}
+
+
 
 
 
